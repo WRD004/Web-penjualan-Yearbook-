f3ca551453d86a4120cfe3522d141b9946be4cf5
WRD004 <23082010234@student.upnjatim.ac.id>
2024-12-29 13:07:22 +0700

    Web Penjualan

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>PT PIXEL MEDIA | Vendor Yearbook</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <div class="header">
+    <p>PT PIXEL MEDIA | VENDOR YEARBOOK</p>
+  </div>
+  <nav>
+    <ul> <?php include("page/navbar.php") ?> </ul>
+  </nav>
+
+  <?php include("content.php") ?>
+
+  <?php include("page/user/keranjang.php") ?>
+  <div class="footer">
+    <p>PT PIXEL MEDIA </p> <p> Jl.Kenangan Indah</p>
+  </div>
+
+</body>
+
+</html>
---
+++ page/tentang.php
@@ -0,0 +1,58 @@
+<div class="box-title">
+    <p>Tentang / <b>Profil Pembuat</b></p>
+</div>
+<div id="box">
+
+  <h1>Tentang</h1>
+  <img src="img/liza.png" width="150" align="left"/>
+  <table>
+    <tr>
+      <ul>
+        <td width="25%">
+          <li><b>NAMA</b></li>
+        </td>
+        <td>: Liza Ayu Yuliandini</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>NIM</b></li>
+        </td>
+        <td>: 23082010003</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>Jurusan</b></li>
+        </td>
+        <td>: S1 Sistem Informasi</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>Hobby</b></li>
+        </td>
+        <td>: Traveling</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>Minat</b></li>
+        </td>
+        <td>:  Jaringan</td>
+      </ul>
+    </tr>
+
+   
+
+  </table>
+
+</div>
